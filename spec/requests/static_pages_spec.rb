ec82e4f13d25fe9b62575ffc4267d0b7d57393af
Mike <mikeh1010@gmail.com>
2014-07-17 18:48:16 -0600

    Add Rspec full_title utility, add Users controller, signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -16,20 +16,20 @@ describe "StaticPages" do
     before { visit help_path }
 
   	it { should have_content('Help') }
-  	it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
+  	it { should have_title(full_title('Help')) }
   end
 
   describe "About page"	do
     before { visit about_path }
 
   	it { should have_content('About Us') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | About") }
+    it { should have_title(full_title('About')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
